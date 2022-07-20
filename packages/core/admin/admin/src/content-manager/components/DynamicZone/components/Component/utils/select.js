430cc8e97723cae4af81aa606e76ca0e0413462b
Stanislav Podolia <stanislavpodolia@gmail.com>
2022-07-20 15:43:12 +0300

    Allow text and relation as main field

--- packages/core/admin/admin/src/content-manager/components/DynamicZone/components/Component/utils/select.js
+++ packages/core/admin/admin/src/content-manager/components/DynamicZone/components/Component/utils/select.js
@@ -1,11 +1,21 @@
+import { useMemo } from 'react';
+import { get, toString } from 'lodash';
 import { useCMEditViewDataManager } from '@strapi/helper-plugin';
 
-function useSelect() {
-  const { moveComponentField, triggerFormValidation } = useCMEditViewDataManager();
+function useSelect({ schema, componentFieldName }) {
+  const { moveComponentField, triggerFormValidation, modifiedData } = useCMEditViewDataManager();
+
+  const mainField = useMemo(() => get(schema, ['settings', 'mainField'], 'id'), [schema]);
+  const mainFieldRelation =  useMemo(() => get(schema, ['metadatas', mainField, 'list', 'mainField', 'name'], null), [schema, mainField]);
+
+  const displayedValue = mainField !== 'id' && mainFieldRelation !== 'id' ? toString(
+    get(modifiedData, [...componentFieldName.split('.'), ...mainField.split('.')].concat(mainFieldRelation ? [mainFieldRelation] : []), '')
+  ) : '';
 
   return {
     moveComponentField,
     triggerFormValidation,
+    displayedValue
   };
 }
 
