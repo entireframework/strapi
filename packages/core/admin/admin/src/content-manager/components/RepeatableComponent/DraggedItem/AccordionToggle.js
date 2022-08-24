42ad50a5725f9edb14011c3aa80d1259f543bfdd
Stanislav Podolia <stanislavpodolia@gmail.com>
2022-08-24 16:04:48 +0300

    Revert "Fix AccordionToggle imports"
    This reverts commit ca74e0cf61be76ecc4434b4a5c3c7e95ae989b22.

--- packages/core/admin/admin/src/content-manager/components/RepeatableComponent/DraggedItem/AccordionToggle.js
+++ packages/core/admin/admin/src/content-manager/components/RepeatableComponent/DraggedItem/AccordionToggle.js
@@ -4,12 +4,10 @@ import DropdownIcon from '@strapi/icons/CarretDown';
 import styled from 'styled-components';
 import { TextButton } from '@strapi/design-system/TextButton';
 import { Typography } from '@strapi/design-system/Typography';
-import { AccordionTypography } from '@strapi/design-system/Accordion';
+import { AccordionTypography, useAccordion, getBackground } from '@strapi/design-system/Accordion';
 import { Flex } from '@strapi/design-system/Flex';
 import { Stack } from '@strapi/design-system/Stack';
 import { Icon } from '@strapi/design-system/Icon';
-import { useAccordion } from './AccordionContext';
-import { getBackground } from './utils';
 
 const ToggleButton = styled(TextButton)`
   text-align: left;
