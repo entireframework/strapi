57bbef8bcf1bd7bf4b450759ca1425c8b8a34914
Stanislav Podolia <stanislavpodolia@gmail.com>
2022-10-10 14:20:13 +0300

    Add responsiveQuality

--- packages/core/upload/server/controllers/validation/admin/settings.js
+++ packages/core/upload/server/controllers/validation/admin/settings.js
@@ -5,6 +5,7 @@ const { yup, validateYupSchema } = require('@strapi/utils');
 const settingsSchema = yup.object({
   sizeOptimization: yup.boolean().required(),
   responsiveDimensions: yup.boolean().required(),
+  responsiveQuality: yup.number().required().min(0).max(100),
 });
 
 module.exports = validateYupSchema(settingsSchema);
