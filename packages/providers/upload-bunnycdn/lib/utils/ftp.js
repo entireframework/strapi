9e286f7dee13982672dcf7a5ec18bf507afd3308
Stanislav Podolia <stanislavpodolia@gmail.com>
2022-11-25 17:13:15 +0200

    ftp: Add uploadTimeout

--- packages/providers/upload-bunnycdn/lib/utils/ftp.js
+++ packages/providers/upload-bunnycdn/lib/utils/ftp.js
@@ -3,7 +3,8 @@
 const Client = require('ftp');
 const queue = require('queue');
 
-const connectTimeout = 60 * 1000;
+const connectTimeout = 60 * 1000; // 1 min
+const uploadTimeout = 5 * 60 * 1000; // 5 min
 
 const ftpQueue = queue({
   concurrency: 1,
@@ -22,22 +23,45 @@ function putFTPFile(options, file, ftpFolder, ftpFileName) {
   const ftp = new Client();
 
   return new Promise((resolve, reject) => {
+    let rejected = false;
+    const rejectFn = (err) => {
+      if (!rejected) {
+        rejected = true;
+        reject(err);
+      }
+    };
+
     console.log('connecting', options, ftpFolder, ftpFileName);
     ftp.on('ready', resolve);
-    ftp.on('error', reject);
+    ftp.on('error', rejectFn);
     ftp.on('close', (hadErr) => {
-      if (hadErr) reject();
+      if (hadErr) rejectFn();
     });
-    setTimeout(reject, connectTimeout);
+    setTimeout(rejectFn, connectTimeout);
 
     ftp.connect(options);
   }).then(() => {
     return new Promise((resolve, reject) => {
+      let rejected = false;
+      const rejectFn = (err) => {
+        if (!rejected) {
+          rejected = true;
+          reject(err);
+        }
+      };
+
       console.log('put', `${ftpFolder}/${ftpFileName}`);
+      ftp.on('ready', resolve);
+      ftp.on('error', rejectFn);
+      ftp.on('close', (hadErr) => {
+        if (hadErr) rejectFn();
+      });
+      setTimeout(rejectFn, uploadTimeout);
+
       ftp.mkdir(ftpFolder, true, () => {
         ftp.put(file, `${ftpFolder}/${ftpFileName}`, (err) => {
           if (err) {
-            reject(err);
+            rejectFn(err);
           }
 
           ftp.end();
@@ -52,21 +76,44 @@ function deleteFTPFile(options, fileName) {
   const ftp = new Client();
 
   return new Promise((resolve, reject) => {
+    let rejected = false;
+    const rejectFn = (err) => {
+      if (!rejected) {
+        rejected = true;
+        reject(err);
+      }
+    };
+
     console.log('connecting', options, fileName);
     ftp.on('ready', resolve);
-    ftp.on('error', reject);
+    ftp.on('error', rejectFn);
     ftp.on('close', (hadErr) => {
-      if (hadErr) reject();
+      if (hadErr) rejectFn();
     });
-    setTimeout(reject, connectTimeout);
+    setTimeout(rejectFn, connectTimeout);
 
     ftp.connect(options);
   }).then(() => {
     return new Promise((resolve, reject) => {
+      let rejected = false;
+      const rejectFn = (err) => {
+        if (!rejected) {
+          rejected = true;
+          reject(err);
+        }
+      };
+
       console.log('delete', fileName);
+      ftp.on('ready', resolve);
+      ftp.on('error', rejectFn);
+      ftp.on('close', (hadErr) => {
+        if (hadErr) rejectFn();
+      });
+      setTimeout(rejectFn, uploadTimeout);
+
       ftp.delete(fileName, (err) => {
         if (err) {
-          reject(err);
+          rejectFn(err);
         }
 
         ftp.end();
